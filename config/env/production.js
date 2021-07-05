c6f6db6cce986c7c8c9b563f51a2ad038f819e49
LeoCR <leonardoas2007@gmail.com>
2021-07-04 18:10:58 -0600

    Fix/Updating Config enviroment

--- config/env/production.js
+++ config/env/production.js
@@ -374,7 +374,7 @@ module.exports = {
   *                                                                         *
   ***************************************************************************/
   custom: {
-    baseUrl: 'https://sailsjs-upload-images.herokuapp.com/',
+    baseUrl: 'https://sailsjs-upload-images.herokuapp.com',
     internalEmailAddress: 'laranibarsanchez@gmail.com',
 
     // sendgridSecret: 'SG.fake.3e0Bn0qSQVnwb1E4qNPz9JZP5vLZYqjh7sn8S93oSHU',
